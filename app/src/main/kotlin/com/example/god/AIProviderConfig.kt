828fb78ddca728c43048b7fd2dd0e8babb975007
aryangod5 <whayssudn@gmail.com>
2026-09-17 17:16:53 +0530

    Delete app/src/main/kotlin/com/example/god/AIProviderConfig.kt

--- app/src/main/kotlin/com/example/god/AIProviderConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.example.god
-
-data class AIProviderConfig(
-    val providerName: String = "",
-    val endpoint: String = "",
-    val apiKey: String = "",
-    val modelName: String = "",
-    val enabled: Boolean = false
-)
